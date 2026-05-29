b644d300408e4e91f640ecb8a98089c1e892cc8b
Giusy-1 <giu.one95@gmail.com>
2026-05-29 12:27:35 +0200

    Update Questionnaire_MongoInterface.java

--- src/main/java/it/unipi/largescale/DiscoverEurope/repository/Questionnaire_MongoInterface.java
+++ src/main/java/it/unipi/largescale/DiscoverEurope/repository/Questionnaire_MongoInterface.java
@@ -8,3 +8,5 @@ import java.util.Optional;
 public interface Questionnaire_MongoInterface extends MongoRepository<Questionnaire, String> {
     Optional<Questionnaire> findTopByUserIdOrderBySubmittedAtDesc(String userId);
 }
+
+List<Questionnaire> findByUserId(String userId);
