7a09231eadf9809b26f38b09d96c22287d2994f6
Giusy-1 <giu.one95@gmail.com>
2026-05-29 20:37:06 +0200

    Update Questionnaire_Controller_Mongo.java

--- src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
+++ src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
@@ -1,6 +1,20 @@
 package it.unipi.largescale.DiscoverEurope.controller;
+@RestController
+@RequestMapping("/api/questionnaires")
 
 public class Questionnaire_Controller_Mongo {
 }
+@Autowired
+    private Questionnaire_Service_Mongo questionnaireService;
 
 List<Questionnaire> userQuestionnaires = questionnaireService.getByUserId(userId);
+
+@PostMapping
+
+  public ResponseEntity<Questionnaire> submitQuestionnaire(@RequestBody QuestionnaireRequest request) {
+    Questionnaire savedQuestionnaire = questionnaireService.saveQuestionnaire(
+                request.getUserId(), 
+                request.getPreferences()
+        );
+    return new ResponseEntity<>(savedQuestionnaire, HttpStatus.CREATED);
+    }
