40dae012324d6cb1db302315c422a1ded1e60758
Giusy-1 <giu.one95@gmail.com>
2026-05-29 12:29:21 +0200

    Update Questionnaire_Controller_Mongo.java

--- src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
+++ src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
@@ -2,3 +2,5 @@ package it.unipi.largescale.DiscoverEurope.controller;
 
 public class Questionnaire_Controller_Mongo {
 }
+
+List<Questionnaire> userQuestionnaires = questionnaireService.getByUserId(userId);
