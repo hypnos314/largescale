bb9645bb3834b9a55ab79dc3c158238229a28f1a
Giusy-1 <giu.one95@gmail.com>
2026-05-29 20:46:05 +0200

    Update Questionnaire_Controller_Mongo.java

--- src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
+++ src/main/java/it/unipi/largescale/DiscoverEurope/controller/Questionnaire_Controller_Mongo.java
@@ -18,3 +18,13 @@ List<Questionnaire> userQuestionnaires = questionnaireService.getByUserId(userId
         );
     return new ResponseEntity<>(savedQuestionnaire, HttpStatus.CREATED);
     }
+
+@GetMapping("/user/{userId}")
+    public ResponseEntity<List<Questionnaire>> getUserQuestionnaires(@PathVariable String userId) {
+        // Chiede al service la lista dei questionari
+        List<Questionnaire> questionnaires = questionnaireService.getByUserId(userId);
+        
+        // Restituisce la lista e lo status code 200 (OK)
+        return ResponseEntity.ok(questionnaires);
+    }
+}
